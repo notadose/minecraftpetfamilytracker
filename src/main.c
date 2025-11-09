159f95546e18459bb535c64d62dc8862cd0e2e45
notadose <>
2025-11-09 16:00:31 -0500

    more changes
    change more code again

--- src/main.c
+++ src/main.c
@@ -67,16 +67,17 @@ int writePet(FILE *of, pet_record *in) {
   return 0;
 }
 
-int addPet(pet_record *out) {
+int readPetDescription(pet_description *out) {
+
   char buf[MAX_ID_LENGTH];
   char idFormat[32];
-  snprintf(idFormat, sizeof(idFormat), "%%s%i\n", MAX_ID_LENGTH - 1);
+  snprintf(idFormat, sizeof(idFormat), "%%s%i", MAX_ID_LENGTH - 1);
 
   char petTypeName[MAX_ID_LENGTH];
   const pet_type *readingType;
 
 start:
-  printf("What kind of pet do you want to add? Options are listed below:\n");
+  printf("What kind of pet is this? Options are listed below:\n");
   for (unsigned short i = 0; i < sizeof(pet_types) / sizeof(*pet_types); i++) {
     printf("%s\n", pet_types[i].name);
   }
@@ -111,27 +112,39 @@ start:
     printf("\n");
   }
 
+  memcpy(&(out->attribs), attribs, sizeof(attribs));
+  strcpy(out->pet_type, readingType->name);
+
+  return 0;
+}
+
+int addPet(pet_record *out) {
+  pet_description desc;
+  if (readPetDescription(&desc) != 0)
+    return 1;
+
   petid parents[2];
-  printf("What is the id of your %s's first parent?\n", readingType->name);
-  if (scanf("%u\n", &(parents[0])) != 1)
+  printf("What is the id of your %s's first parent? (type -1 for no parent)\n", desc.pet_type);
+  if (scanf("%u", &(parents[0])) != 1)
     return reterr("Error reading parent id from input");
   printf("\n");
 
-  printf("What is the id of your %s's second parent?\n", readingType->name);
-  if (scanf("%u\n", &(parents[1])) != 1)
+  printf("What is the id of your %s's second parent? (type -1 for no parent)\n", desc.pet_type);
+  if (scanf("%u", &(parents[1])) != 1)
     return reterr("Error reading parent id from input");
   printf("\n");
 
   memcpy(&(out->parents), parents, sizeof(parents));
-  memcpy(&(out->description.attribs), attribs, sizeof(attribs));
-  strcpy(out->description.pet_type, readingType->name);
+  memcpy(&(out->description.attribs), desc.attribs, sizeof(desc.attribs));
+  strcpy(out->description.pet_type, desc.pet_type);
 
-  printf("Added your %s to the registry!\n", readingType->name);
+  printf("Added your %s to the registry!\n\n", desc.pet_type);
 
   return 0;
 }
 
 void listPets(pet_record *records, unsigned int records_count) {
+  printf("\n");
   for (unsigned int i = 0; i < records_count; i++) {
     pet_record *r = &(records[i]);
     const pet_type *petType;
@@ -144,6 +157,31 @@ void listPets(pet_record *records, unsigned int records_count) {
 
     printf("Parents %u and %u\n", r->parents[0], r->parents[1]);
   }
+  printf("\n");
+}
+
+unsigned int findPet(pet_record *records, unsigned int records_count) {
+  pet_description d;
+  readPetDescription(&d);
+
+  for (unsigned int i = 0; i < records_count; i++) {
+    pet_record *r = &(records[i]);
+    const pet_type *petType;
+    getPetType(r->description.pet_type, &petType);
+    if (strcmp(petType->name, d.pet_type) != 0)
+      continue;
+
+    for (unsigned short i2 = 0; i2 < petType->attribs_count; i2++) {
+      if (strcmp(r->description.attribs[i2], d.attribs[i2]) != 0)
+        goto cmp_next;
+    }
+
+    return i;
+
+  cmp_next:
+  }
+
+  return records_count;
 }
 
 int main(int argc, char *argv[]) {
@@ -175,13 +213,21 @@ int main(int argc, char *argv[]) {
     } else if (strcmp(cmd, "addpet") == 0) {
       addPet(&(records[record_idx]));
       record_idx++;
-    } else {
-      printf("Editing %s\n"
-             "Use listpets to show the pet registry.\n"
-             "Use addpet to add a pet record.\n"
-             "Use exit to close and save\n",
-             filename);
+    } else if (strcmp(cmd, "findpet") == 0) {
+      unsigned int i = findPet(records, record_idx);
+      if (i == record_idx) {
+        printf("Sorry, that pet wasn't found :(\n");
+      } else {
+        printf("I found your pet! it has ID %i\n", i);
+      }
     }
+
+    printf("Editing %s\n"
+           "Use listpets to show the pet registry.\n"
+           "Use addpet to add a pet record.\n"
+           "Use exit to close and save\n"
+           "Use findpet to find a pet's id based on its description\n",
+           filename);
 
     scanf("%s63\n", cmd);
   }
